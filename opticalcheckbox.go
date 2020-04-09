877b4d461b030d1637b13ef4d8f4653d0b6facaa
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-09 01:29:36 +0100

    added dataBox (returns subImage)

--- opticalcheckbox.go
+++ opticalcheckbox.go
@@ -13,7 +13,7 @@ type Box struct {
 	Bounds  image.Rectangle
 }
 
-func CheckImageFile(inputPath string, boxes []Box) ([]bool, error) {
+func CheckBoxFile(inputPath string, boxes []Box) ([]bool, error) {
 
 	var results []bool
 
@@ -37,6 +37,36 @@ func CheckImageFile(inputPath string, boxes []Box) ([]bool, error) {
 	}
 
 	return results, nil
+}
+
+func DataBoxFile(inputPath string, boxes []Box) ([]bool, []image.Image, error) {
+
+	var (
+		results []bool
+		images  []image.Image
+	)
+
+	reader, err := os.Open(inputPath)
+
+	if err != nil {
+		return []bool{}, []image.Image{}, err
+	}
+
+	defer reader.Close()
+
+	wholeImage, _, err := image.Decode(reader)
+
+	if err != nil {
+		return []bool{}, []image.Image{}, err
+	}
+
+	for idx := 0; idx < len(boxes); idx = idx + 1 {
+		result, img := DataBox(wholeImage, boxes[idx])
+		results = append(results, result)
+		images = append(images, img)
+	}
+
+	return results, images, nil
 }
 
 //see https://stackoverflow.com/questions/16072910/trouble-getting-a-subimage-of-an-image-in-go
@@ -90,4 +120,29 @@ func CheckBox(im image.Image, box Box) bool {
 	} else {
 		return averagePixelValue > 1.0
 	}
+}
+
+func DataBox(im image.Image, box Box) (bool, image.Image) {
+
+	checkImage := im.(SubImager).SubImage(box.Bounds)
+	cum := uint32(0)
+	bounds := checkImage.Bounds()
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := checkImage.At(x, y).RGBA()
+			cum = cum + r>>11 + g>>11 + b>>11
+		}
+	}
+
+	colourCount := 3
+	pixelCount := colourCount * (bounds.Max.X - bounds.Min.X) * (bounds.Max.Y - bounds.Min.Y)
+	averagePixelValue := float64(cum / uint32(pixelCount))
+
+	if box.Vanilla {
+		return averagePixelValue < 30.0, checkImage
+	} else {
+		return averagePixelValue > 1.0, checkImage
+	}
+
 }
--- opticalcheckbox_test.go
+++ opticalcheckbox_test.go
@@ -1,6 +1,7 @@
 package opticalcheckbox
 
 import (
+	"fmt"
 	"image"
 	jpeg "image/jpeg"
 	_ "image/png"
@@ -114,15 +115,68 @@ func TestCheckBox(t *testing.T) {
 
 }
 
-func TestCheckImageFile(t *testing.T) {
+func TestDataBox(t *testing.T) {
 
-	results, err := CheckImageFile("./img/test.png", testCheckBoxes)
+	reader, err := os.Open("./img/test.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	testImage, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for idx := 0; idx < len(testCheckBoxes); idx = idx + 1 {
+		actual, _ := DataBox(testImage, testCheckBoxes[idx])
+		wanted := expectedBox[idx]
+		if actual != wanted {
+			t.Errorf("Unexpected result for checkbox %d; got %v wanted %v\n",
+				idx, actual, wanted)
+		}
+
+	}
+
+}
+
+func TestCheckBoxFile(t *testing.T) {
+
+	results, err := CheckBoxFile("./img/test.png", testCheckBoxes)
 
 	if err != nil {
 		t.Errorf("error %v\n", err)
 	}
 
 	for idx, result := range results {
+
+		if result != expectedBox[idx] {
+			t.Errorf("Unexpected result for checkbox %d; got %v wanted %v\n",
+				idx, result, expectedBox[idx])
+		}
+
+	}
+}
+
+func TestDataBoxFile(t *testing.T) {
+
+	results, images, err := DataBoxFile("./img/test.png", testCheckBoxes)
+
+	if err != nil {
+		t.Errorf("error %v\n", err)
+	}
+
+	for idx, result := range results {
+
+		of, err := os.Create(fmt.Sprintf("dataBox%d.jpg", idx))
+
+		if err != nil {
+			t.Errorf("problem saving failed checkbox image to file %v\n", err)
+		}
+
+		defer of.Close()
+
+		err = jpeg.Encode(of, images[idx], nil)
 
 		if result != expectedBox[idx] {
 			t.Errorf("Unexpected result for checkbox %d; got %v wanted %v\n",
